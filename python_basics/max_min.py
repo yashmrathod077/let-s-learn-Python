5e7f54b3b37e9aedbbcfcf745ef040415b6a0c5e
yashmrathod077 <yashmrathod077@gmail.com>
2026-04-03 10:41:25 +0530

    check bigest number

---
+++ python_basics/max_min.py
@@ -0,0 +1,8 @@
+n1 = int(input("enter a number "))
+n2 = int(input("enter another number "))
+
+if n1 > n2:
+    print("the number is greater than the number")
+    else:
+    print("the number is less than the number")
+
