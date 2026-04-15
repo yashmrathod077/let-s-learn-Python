89b1ce200b03054fcc79be513a256c81ec4e5085
yashmrathod077 <yashmrathod077@gmail.com>
2026-04-15 07:02:32 +0530

    Shairy

--- python_basics/Shairy.py
+++ python_basics/Shairy.py
@@ -1,9 +1,9 @@
 print("""College mein milte hain roz, par baat nahi kar paate,
-Doston ke beech rehkar bhi jazbaat nahi jataate.
-Nazrein milti hain chupke se, phir hum jhuk jaate hain,
-Itne paas hoke bhi jaise door reh jaate hain.
-Haath pakadne ka mann toh roz karta hai,
-Par duniya ke darr se khud ko rok lete hain.
-Sach toh ye hai…
-Hum dono ek doosre ke hain,
-Par duniya ke saamne anjaan ban jaate hain. ❤️""");
+          Doston ke beech rehkar bhi jazbaat nahi jataate.
+          Nazrein milti hain chupke se, phir hum jhuk jaate hain,
+          Itne paas hoke bhi jaise door reh jaate hain.
+          Haath pakadne ka mann toh roz karta hai,
+          Par duniya ke darr se khud ko rok lete hain.
+          Sach toh ye hai…
+          Hum dono ek doosre ke hain,
+          Par duniya ke saamne anjaan ban jaate hain. ❤️""");
